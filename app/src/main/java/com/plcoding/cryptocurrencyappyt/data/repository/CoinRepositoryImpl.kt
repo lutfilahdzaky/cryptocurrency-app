71a9b594a94151675134c5de0af3575feaea5c7d
Lutfilah Dzaky <lutfilahdzaky12@gmail.com>
2022-05-15 08:10:10 +0700

    Refactor response handling to data layer (repository)
    2 little corrections from my side:
    
    1. In this video I mapped the DTO to a model in the domain layer. You should rather do this in the data layer (in the repository)
    2. The response handling should also rather be done in the repository (catching HttpException & IOException) as this is specific to Retrofit and the domain layer shouldn't know about that.
    
    Sorry that I didn't show that in the video, but I also never stop learning :)
    
    Q&A
    if we do response handling in repository then what the hell use-case do ?
    Just everything that's in the use case can be put in the repo and the use case calls the repo function and does nothing else. The use case seems unnecessary then, but it often contains additional business logic like validation or so.

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/data/repository/CoinRepositoryImpl.kt
@@ -1,22 +1,39 @@
 package com.plcoding.cryptocurrencyappyt.data.repository
 
+import com.plcoding.cryptocurrencyappyt.common.Resource
 import com.plcoding.cryptocurrencyappyt.data.remote.CoinPaprikaApi
 import com.plcoding.cryptocurrencyappyt.data.remote.dto.CoinDetailDto
 import com.plcoding.cryptocurrencyappyt.data.remote.dto.CoinDto
 import com.plcoding.cryptocurrencyappyt.domain.model.Coin
 import com.plcoding.cryptocurrencyappyt.domain.model.CoinDetail
 import com.plcoding.cryptocurrencyappyt.domain.repository.CoinRepository
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
 class CoinRepositoryImpl @Inject constructor(
     private val api: CoinPaprikaApi
 ) : CoinRepository {
-    override suspend fun getCoins(): List<CoinDto> {
-        return api.getCoins()
+    override suspend fun getCoins(): Resource<List<Coin>> {
+        return try {
+            val coins = api.getCoins().map { it.toCoin() }
+            Resource.Success(coins)
+        } catch (e: HttpException) {
+            Resource.Error(e.localizedMessage ?: "An unexpected error occurred")
+        } catch (e: IOException) {
+            Resource.Error("Couldn't reach server. Check your internet connection")
+        }
     }
 
-    override suspend fun getCoinById(coinId: String): CoinDetailDto {
-        return api.getCoinById(coinId)
+    override suspend fun getCoinById(coinId: String): Resource<CoinDetail> {
+        return try {
+            val coin = api.getCoinById(coinId).toCoinDetail()
+            Resource.Success<CoinDetail>(coin)
+        } catch (e: HttpException) {
+            Resource.Error<CoinDetail>(e.localizedMessage ?: "An unexpected error occurred")
+        } catch (e: IOException) {
+            Resource.Error<CoinDetail>("Couldn't reach server. Check your internet connection")
+        }
     }
 }
 
